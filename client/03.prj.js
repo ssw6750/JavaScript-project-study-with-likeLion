8664e78405d24dfc4d03a9de7154cc0798cffc71
ssw6750 <skowko6750@naver.com>
2023-01-15 15:28:43 +0900

    프로젝트3 캐로셀 리팩토링

--- client/03.prj.js
+++ client/03.prj.js
@@ -9,12 +9,15 @@ let imgs = [
 
 const numberOfImagesInCarousel = 3;
 const imgWidth = 500;
-const carouselGap = 10;
+const imgHeight = 400;
+const carouselGap = 30;
 const carouselSpace = imgWidth+carouselGap;
 const carousel_imgs = document.querySelector('.carousel_imgs')
 const container = document.querySelector('.container')
 const carousel__left_btn = document.querySelector('.carousel__left_btn')
 const carousel__right_btn = document.querySelector('.carousel__right_btn')
+const carousel_wrapper = document.querySelector('.carousel_wrapper')
+
 
 // 화살표함수는 호이스팅이 발생하지 않음
 const checkIndex = () => {
@@ -35,14 +38,18 @@ const checkIndex = () => {
 
 //너무 직접적으로 할당하는 느낌... 태그내에서 dataset 선언은 더블쿼토 필요...왜와이
 // 인덱스가 스트링이로 입력됨
+carousel_wrapper.style.width=`${numberOfImagesInCarousel*imgWidth+carouselGap*(numberOfImagesInCarousel-1)}px`
+carousel_wrapper.style.height=`${imgHeight}px`;
 container.dataset.index=0
 carousel_imgs.style.left = -carouselSpace*(container.dataset.index)
+carousel_imgs.style.gap= `${carouselGap}px`
 checkIndex()
 
 
 imgs.map(({url}, index)=>{
   const img_tmp = document.createElement('div') 
   img_tmp.classList.add('carousel_img')
+  img_tmp.style.width=`${imgWidth}px`;
   img_tmp.style.backgroundImage=`url(${url})`
   img_tmp.dataset.index=index;
   img_tmp.dataset.name='carousel_img';
@@ -69,10 +76,6 @@ const click_handler = (e) => {
     container.dataset.index=+(container.dataset.index)+1
     checkIndex()
   }
-  if (target.dataset.name==='carousel_img') {
-    console.log('carousel_img');
-    console.log(target.dataset.index);
-  }
 }
 
 const mousedown_handler = (e) => {
@@ -88,7 +91,9 @@ const mousedown_handler = (e) => {
   }
 
   if (target.dataset.name === 'carousel_img') {
+    //carousel_imgs의 transition만 바꾸는 법 모르겠음,,,
     carousel_imgs.className='carousel_transition'
+    carousel_imgs.style.gap= `${carouselGap}px`
     container.dataset.offset = e.clientX
     container.dataset.leftValue= parseInt(carousel_imgs.style.left)
     document.body.addEventListener("mousemove", mousemove_handler)
@@ -97,6 +102,7 @@ const mousedown_handler = (e) => {
 
 const mouseup_handler = (e) => {
   carousel_imgs.className='carousel_imgs'
+  carousel_imgs.style.gap= `${carouselGap}px`
   if (parseInt(carousel_imgs.style.left) > 0) {;
     container.dataset.index=0
   } else if (parseInt(carousel_imgs.style.left) < -carouselSpace*(imgs.length-numberOfImagesInCarousel)) {
@@ -111,7 +117,6 @@ const mouseup_handler = (e) => {
 
 const mousemove_handler = (e) => {
   carousel_imgs.style.left = `${(container.dataset.leftValue - (container.dataset.offset-e.clientX))}px`
-
 }
 
 
@@ -122,3 +127,6 @@ document.body.addEventListener("mouseup", mouseup_handler)
 document.body.addEventListener("mouseleave", mouseup_handler)
 
 // 이벤트가 자기 자신을 해체할 수 있나..?
+// scrollleft ?
+//let outer = gallery.getBoundingClientRect();
+//let inner = carousel.getBoundingClientRect();
