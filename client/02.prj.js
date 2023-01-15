6331466c44c1a8c9c4e7fe557c022f36b2211aa7
ssw6750 <skowko6750@naver.com>
2023-01-15 11:03:34 +0900

    프로젝트 2수정

--- client/02.prj.js
+++ client/02.prj.js
@@ -18,14 +18,12 @@ const light_box__left_btn = document.querySelector('.light_box__left_btn')
 const light_box__right_btn = document.querySelector('.light_box__right_btn')
 
 
-
-
 //구조분해 할당
 imgs.map(({url}, index) => {
   const img_box = document.createElement('div')
   // 노드의 클래스 접근
   img_box.className='img_box'
-  img_box.index = index // 이렇게 해도 괜찮은가? 오염시키는 느낌?
+  img_box.index = index // 이렇게 해도 괜찮은가? 오염시키는 느낌? 이거 dataset써야함
   img_box.style.backgroundImage = `url(${url})`
   img_box.addEventListener("click", imgbox_click);
 
@@ -36,8 +34,26 @@ light_box__close.addEventListener("click", light_box__close_click)
 light_box__left_btn.addEventListener("click", light_box__left_btn_click)
 light_box__right_btn.addEventListener("click", light_box__right_btn_click)
 
-const darkFiltering = document.createElement('div')
+
+// const darkFiltering = document.createElement('div')
+// darkFiltering.className='dark_filtering'
+
+// 클로저 함수 제대로 쓴거 맞음??
+function darkFiltering_click(e) {
+    close()
+}
+
+function darkFilteringClouser(event) {
+  const darkFiltering = document.createElement('div')
   darkFiltering.className='dark_filtering'
+  darkFiltering.addEventListener("click", event)
+  return function() {
+    return darkFiltering
+  }
+}
+const darkFiltering = darkFilteringClouser(darkFiltering_click)
+
+
 
 // 이미지 박스 클릭 이벤트
 function imgbox_click(e) {
@@ -45,25 +61,21 @@ function imgbox_click(e) {
   light_box.index=e.target.index;
   light_box__img.style.backgroundImage=`url(${imgs[e.target.index].url})`
   check_mark_arrow()
-  console.log(light_box.index);
 
   //인덱싱
   light_box__index.innerHTML = `Image ${e.target.index+1} of ${imgs.length}`
 
-  body.appendChild(darkFiltering)
+  body.appendChild(darkFiltering())
 }
 
 function light_box__close_click(e) {
-  light_box.style.display='none'
-  body.removeChild(darkFiltering)
+  close()
 }
-
 
 // 좀 더 클린코드로 작성할 수 있을것 같은데?
 function light_box__left_btn_click(e) {
   (light_box.index > 0)&&(light_box__img.style.backgroundImage=`url(${imgs[--light_box.index].url})`)
   check_mark_arrow()
-
 }
 
 function light_box__right_btn_click(e) {
@@ -72,18 +84,22 @@ function light_box__right_btn_click(e) {
 }
 
 function check_mark_arrow() {
-  if(light_box.index === 0) {light_box__img.removeChild(light_box__left_btn)}
-  if(light_box.index === 1) {light_box__img.appendChild(light_box__left_btn)}
-  if(light_box.index === imgs.length-1) {light_box__img.removeChild(light_box__right_btn)}
-  if(light_box.index === imgs.length-2) {light_box__img.appendChild(light_box__right_btn)}
+  if(light_box.index === 0) {
+    light_box__left_btn.style.display = 'none'
+    light_box__right_btn.style.display = 'flex'}
+  if(light_box.index >= 1 && light_box.index <= imgs.length-2) {
+    light_box__left_btn.style.display = 'flex'
+    light_box__right_btn.style.display = 'flex'
+  }
+  if(light_box.index === imgs.length-1) {
+    light_box__right_btn.style.display = 'none'
+    light_box__left_btn.style.display = 'flex'
+  }
 }
 
-
-
-
-
-
-
-
+function close(){
+  light_box.style.display='none'
+  body.removeChild(darkFiltering())
+}
 
 
